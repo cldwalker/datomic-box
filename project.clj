589338e9a0fdc2c7fb6043226b1ecf3d6cdeab48
Gabriel Horner <gabriel.horner@gmail.com>
2013-06-03 08:47:23 -0400

    copy over working spec from spike repo

--- project.clj
+++ project.clj
@@ -3,7 +3,28 @@
   :url "http://github.com/cldwalker/datomic-box"
   :license {:name "The MIT License"
             :url "https://en.wikipedia.org/wiki/MIT_License"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 ;; Can't do 0.8.0-beta10
+                 ;; datomic-crate error - ExceptionInfo Invalid remote-file-arguments: Path [:service-impl] was not specified in the schema.  clojure.core/ex-info (core.clj:4327)
+                 [com.palletops/pallet "0.8.0-beta.8"]
+                 [org.cloudhoist/pallet-jclouds "1.5.2"]
+                 [org.clojars.strad/datomic-crate "0.8.6"]
+                 [com.palletops/java-crate "0.8.0-beta.5"]
+                 [com.palletops/upstart-crate "0.8.0-alpha.2"]
+
+                 ;; To get started we include all jclouds compute providers.
+                 ;; You may wish to replace this with the specific jclouds
+                 ;; providers you use, to reduce dependency sizes.
+                 [org.jclouds/jclouds-allblobstore "1.5.5"]
+                 [org.jclouds/jclouds-allcompute "1.5.5"]
+                 [org.jclouds.driver/jclouds-slf4j "1.5.5"
+                  ;; the declared version is old and can overrule the
+                  ;; resolved version
+                  :exclusions [org.slf4j/slf4j-api]]
+                 [org.jclouds.driver/jclouds-sshj "1.5.5"]
+                 [ch.qos.logback/logback-classic "1.0.9"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+  :repositories
+  {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"} 
   :aliases {"all" ["with-profile" "dev:dev,1.4"]}
   :test-selectors {:focus :focus :default (constantly true)})
--- src/datomic_box/core.clj
+++ src/datomic_box/core.clj
@@ -1,1 +1,40 @@
-(ns datomic-box.core)
+(ns datomic-box.core
+  (:require
+    [pallet.api :as api]
+    [pallet.configure :as configure]
+    [pallet.crate.automated-admin-user :as admin-user]
+    [pallet.crate.java :as java]
+    [pallet.crate.datomic :as datomic]
+    [pallet.crate.upstart :as upstart]))
+
+(def default-node-spec
+  (api/node-spec
+   :image {:os-family :ubuntu :image-id "us-east-1/ami-9c78c0f5"}
+    ;; Using a small since datomic defaults to using a 1GB ram
+   :hardware {:min-cores 1 :hardware-id "m1.small" }))
+
+(def
+  ^{:doc "Defines the type of node datomic-box will run on"}
+  base-server
+  (api/server-spec
+   :phases
+   {:bootstrap (api/plan-fn (admin-user/automated-admin-user))}))
+
+(def project-spec
+  (api/group-spec "datomic-box"
+   :extends [base-server
+             ;; TODO: Use openjdk. Currently ubuntu 12 ships with a broken openjdk.
+             ;; Need to update packages to unbreak it.
+             (java/server-spec {:vendor :oracle :version [7]})
+             (datomic/datomic {})
+             (upstart/server-spec {})]
+   :count 1
+   :node-spec default-node-spec))
+
+(comment
+  ;; Use api/converge when creating your box for the first time.
+  ;; For future invocations on your box, use api/lift.
+  (api/converge
+    project-spec 
+    :phase :install
+    :compute (configure/compute-service :aws))) 
